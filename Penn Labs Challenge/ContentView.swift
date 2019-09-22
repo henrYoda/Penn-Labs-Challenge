93f422d7dd18a94fbd8b7b4ecd31788b5ac2e588
Henrique <henrique.lorente@gmail.com>
2019-09-22 01:10:28 -0400

    Initial Commit

---
+++ Penn Labs Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Penn Labs Challenge
+//
+//  Created by Henrique Lorente on 9/22/19.
+//  Copyright © 2019 Henrique Lorente. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Penn Labs ChallengeTests/Penn_Labs_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Penn_Labs_ChallengeTests.swift
+//  Penn Labs ChallengeTests
+//
+//  Created by Henrique Lorente on 9/22/19.
+//  Copyright © 2019 Henrique Lorente. All rights reserved.
+//
+
+import XCTest
+@testable import Penn_Labs_Challenge
+
+class Penn_Labs_ChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
